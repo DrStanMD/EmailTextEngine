b552a6a28f300fb501d3af80847c61da29f41e76
linbilly <billy.lin@gmail.com>
2015-03-05 13:36:33 -0800

    updating version

--- GreaseMonkey/EncounterNoteEmailer.user.js
+++ GreaseMonkey/EncounterNoteEmailer.user.js
@@ -3,7 +3,7 @@
 // @namespace   http://dev.drbillylin.com/oscarwiki
 // @description	Puts ENEngine-enabled Email and Text Buttons on the Encounter Note page
 // @include     */casemgmt/forward.jsp?action=view*
-// @version     1.2
+// @version     1.21
 // @grant       none
 // @updateURL	https://github.com/linbilly/EmailTextEngine/raw/master/GreaseMonkey/EncounterNoteEmailer.meta.js
 // @downloadURL	https://github.com/linbilly/EmailTextEngine/raw/master/GreaseMonkey/EncounterNoteEmailer.user.js
